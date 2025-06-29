b7789a11d92f04f72c488fbfc3f1fbe63e6d2c3c
jichunwei <49595272@qq.com>
2025-06-29 14:34:40 +0800

    update

---
+++ Runtime/Scripts/Core/Network/INetMessageRegister.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JFramework
+{
+    public interface INetMessageRegister
+    {
+        Dictionary<int, Type> GetAllTables();
+    }
+}
--- Runtime/Scripts/Core/Network/JNetMessageJsonTypeResolver.cs
+++ Runtime/Scripts/Core/Network/JNetMessageJsonTypeResolver.cs
@@ -4,16 +4,36 @@ using System.Text;
 
 namespace JFramework
 {
-   /// <summary>
-   /// 用json格式解析数据的方式获取数据类型
-   /// </summary>
+    /// <summary>
+    /// 用json格式解析数据的方式获取数据类型
+    /// </summary>
     public class JNetMessageJsonTypeResolver : IMessageTypeResolver
     {
         private readonly Dictionary<int, Type> messageTypes = new Dictionary<int, Type>();
 
         IDeserializer deserializer;
 
-        public JNetMessageJsonTypeResolver(IDeserializer deserializer) { this.deserializer = deserializer; }
+        public JNetMessageJsonTypeResolver(IDeserializer deserializer) : this(deserializer, null) { }
+        public JNetMessageJsonTypeResolver(IDeserializer deserializer, INetMessageRegister register)
+        {
+            this.deserializer = deserializer;
+
+            if (register != null)
+            {
+                var dic = register.GetAllTables();
+                foreach (var table in dic)
+                {
+                    RegisterMessageType(table.Key, table.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="messageId"></param>
+        /// <param name="messageType"></param>
+        /// <returns></returns>
         public IMessageTypeResolver RegisterMessageType(int messageId, Type messageType)
         {
             messageTypes[messageId] = messageType;
